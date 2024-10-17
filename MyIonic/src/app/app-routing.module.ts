06021991b3bb088db82d2669bf2319fdb5ef70df
Jayrald <jayraldtajanlangit123@gmail.com>
2024-10-17 09:32:41 +0900

    Article Page
    Creating Article Page

--- MyIonic/src/app/app-routing.module.ts
+++ MyIonic/src/app/app-routing.module.ts
@@ -4,12 +4,20 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'home-page',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'article',
+    loadChildren: () => import('./article/article.module').then(m => m.ArticlePageModule)
   },
 ];
 
